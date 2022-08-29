1d4acbc84678d33e7fea530800d26ffab2489b94
Phanendra Badugu <badugu.phanendra@gmail.com>
2022-08-29 23:26:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from "./Components/SearchBar";
+import VideoList from "./Components/VideoList";
+import VideoDetails from "./Components/VideoDetails";
+
+import { Grid } from "@material-ui/core";
+
+//import youtube from "./Api/youtube";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={12}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              <SearchBar />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetails />
+            </Grid>
+            <Grid item xs={12}>
+              <VideoList />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+    state = {
+        searchTerm: " ",
+    }
+    render(){
+        return(
+            <h1>Search Component</h1>
+        )
+    }
+}
+
+export default SearchBar;
